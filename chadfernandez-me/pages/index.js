e8702a6abf3d9ba13399a6c4b8e961a63a43bcc0
Chad Fernandez <chadfernandez@duck.com>
2022-08-02 20:39:32 +0800

    update `index.js` to reflect dev status of site

--- chadfernandez-me/pages/index.js
+++ chadfernandez-me/pages/index.js
@@ -1,5 +1,6 @@
 
 import Head from 'next/head';
+import Image from 'next/image';
 
 
 function Index(){
@@ -8,7 +9,30 @@ function Index(){
             <Head>
                 <title>Chad Fernandez, 🇵🇭, Student</title>
             </Head>
-            <h1>Welcome to Next.js</h1>
+            <div className='flex flex-col items-center justify-center h-screen m-auto bg-gradient-to-r from-[#FB8D89] via-[#FEF085] to-[#A9DAC8] backdrop-blug-xl animate-gradient-xy'>
+                <div className='px-4 py-8 border-2 border-[#FFFCFF] bg-[#FFFCFF] shadow-2xl text-[#50514F] font-medium rounded-3xl'>
+                    <div className='flex justify-center'>
+                        <Image src={'/android-chrome-512x512.png'} width={70} height={70} />
+                    </div>
+                    <div className='flex justify-center mt-5'>
+                        <p className='w-[60vh]'>
+                            Sorry,
+                            <br />
+                            <br />
+                            My website is not ready yet, I'll update this page as new things are
+                            up and running.
+                            <br />
+                            <br />
+                            You can visit this website's repository at{' '}
+                            <a className='hover:text-sky-500 text-sky-700' href="https://github.com/chof64/chof64">github.com/chof64/chof64</a>{' '}
+                            to keep track of my progress.
+                            <br />
+                            <br />
+                            All the best for now!
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
