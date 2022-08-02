64c00f39aa77500f6a754cd2b7c204e9c02caa7b
Chad Fernandez <chadfernandez@duck.com>
2022-08-02 15:57:14 +0800

    addition of favicons with different sizes

--- chadfernandez-me/pages/index.js
+++ chadfernandez-me/pages/index.js
@@ -1,9 +1,13 @@
 
+import Head from 'next/head';
 
 
 function Index(){
     return(
         <>
+            <Head>
+                <title>Chad Fernandez, 🇵🇭, Student</title>
+            </Head>
             <h1>Welcome to Next.js</h1>
         </>
     )
