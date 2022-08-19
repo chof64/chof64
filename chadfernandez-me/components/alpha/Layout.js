58742c904d6a05645f6980333eaebdde8b619369
Chad Fernandez <chadfernandez@duck.com>
2022-08-19 15:16:33 +0800

    linted + formatted with next lint and prettier

--- chadfernandez-me/components/alpha/Layout.js
+++ chadfernandez-me/components/alpha/Layout.js
@@ -1,18 +1,16 @@
 import Navigation from "./Navigation";
 
-function Layout({ children }){
-  return(
+function Layout({ children }) {
+  return (
     <main className={"subpixel-antialiased"}>
       <div className={"sticky top-0 z-[100]"}>
         <Navigation />
       </div>
       <div className={"flex flex-col items-center"}>
-        <div className={"w-[90vw] md:w-[80vw]"}>
-          {children}
-        </div>
+        <div className={"w-[90vw] md:w-[80vw]"}>{children}</div>
       </div>
     </main>
-  )
+  );
 }
 
 export default Layout;
--- chadfernandez-me/components/alpha/Navigation.js
+++ chadfernandez-me/components/alpha/Navigation.js
@@ -1,20 +1,19 @@
-import Link from 'next/link';
+import Link from "next/link";
 import { Menu } from "@headlessui/react";
 import { forwardRef } from "react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
-function Navigation(){
-
+function Navigation() {
   const navigation = [
-    {"name": "Home", "href": "/"},
-    {"name": "About", "href": "#"},
-    {"name": "Contact", "href": "#"},
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Contact", href: "#" },
   ];
 
   const MenuLink = forwardRef((props, ref) => {
     MenuLink.displayName = "MenuLink";
     let { href, children, ...rest } = props;
-    
+
     return (
       <Link href={href}>
         <a ref={ref} {...rest}>
@@ -24,52 +23,75 @@ function Navigation(){
     );
   });
 
-  return(
-    <>
-      <Menu>
-        {({ open }) => (
-          <div className={"mb-4"}>
-            <div className={"flex justify-center py-2 border-b"}>
-              <div className={"flex justify-between items-center w-[90vw] md:w-[80vw]"}>
-                
-                <div>
-                  <Link href="/"><h1 className={"font-medium cursor-pointer"}>Chad Fernandez</h1></Link>
-                </div>
-                
-                <div>
-                  <nav>
-                    <Menu.Button as={"div"} className={"md:hidden"}>
-                      {open ? (
-                        <XIcon className="w-6 h-6" />
-                      ) : (
-                        <MenuIcon className="w-6 h-6" />
-                      )}
-                    </Menu.Button>
-                    <div className={"hidden md:flex items-center"}>
-                      {navigation.map((item) =>(
-                        <Link href={item.href}><a className={"text-sm hover:bg-blue-200 bg-white px-4 py-1 border-[0.5px] first:border-r-0 first:rounded-l-lg last:border-l-0 last:rounded-r-lg"}>{item.name}</a></Link>
-                      ))}
-                    </div>
-                  </nav>
-                </div>
-                
+  return (
+    <Menu>
+      {({ open }) => (
+        <div className={"mb-4"}>
+          <div className={"flex justify-center border-b py-2"}>
+            <div
+              className={
+                "flex w-[90vw] items-center justify-between md:w-[80vw]"
+              }
+            >
+              <div>
+                <Link href="/">
+                  <h1 className={"cursor-pointer font-medium"}>
+                    Chad Fernandez
+                  </h1>
+                </Link>
+              </div>
+
+              <div>
+                <nav>
+                  <Menu.Button as={"div"} className={"md:hidden"}>
+                    {open ? (
+                      <XIcon className="w-6 h-6" />
+                    ) : (
+                      <MenuIcon className="w-6 h-6" />
+                    )}
+                  </Menu.Button>
+                  <div className={"hidden items-center md:flex"}>
+                    {navigation.map((item, index) => (
+                      <Link href={item.href} key={index}>
+                        <a
+                          className={
+                            "border-[0.5px] bg-white px-4 py-1 text-sm first:rounded-l-lg first:border-r-0 last:rounded-r-lg last:border-l-0 hover:bg-blue-200"
+                          }
+                        >
+                          {item.name}
+                        </a>
+                      </Link>
+                    ))}
+                  </div>
+                </nav>
               </div>
             </div>
-
-            <Menu.Items as={"div"} className={"flex justify-center md:hidden absolute max-h-[60vh] w-full bg-slate-50 rounded-b-xl border-b-[0.5px] shadow-sm"}>
-              <div className={"flex flex-col w-[90vw] my-2"}>
-                {navigation.map((item) => (
-                  <Menu.Item>
-                    <MenuLink className={"bg-white shadow-sm py-3 pl-2 border rounded-xl my-0.5 text-sm font-semibold"} href={item.href}>{item.name}</MenuLink>
-                  </Menu.Item>
-                ))}
-              </div>
-            </Menu.Items>
           </div>
-        )}
-      </Menu>
-    </>
+          <Menu.Items
+            as={"div"}
+            className={
+              "absolute flex max-h-[60vh] w-full justify-center rounded-b-xl border-b-[0.5px] bg-slate-50 shadow-sm md:hidden"
+            }
+          >
+            <div className={"my-2 flex w-[90vw] flex-col"}>
+              {navigation.map((item, index) => (
+                <Menu.Item key={index}>
+                  <MenuLink
+                    className={
+                      "my-0.5 rounded-xl border bg-white py-3 pl-2 text-sm font-semibold shadow-sm"
+                    }
+                    href={item.href}
+                  >
+                    {item.name}
+                  </MenuLink>
+                </Menu.Item>
+              ))}
+            </div>
+          </Menu.Items>
+        </div>
+      )}
+    </Menu>
   );
-};
+}
 
 export default Navigation;
