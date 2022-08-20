e674ee605af6f490c88eb6c45da45019f178e047
Chad Fernandez <chadfernandez@duck.com>
2022-08-20 09:46:06 +0800

    minor buttons and alert update

--- chadfernandez-me/pages/index.js
+++ chadfernandez-me/pages/index.js
@@ -10,7 +10,7 @@ function Index() {
 
   return (
     <div>
-      <div className="px-4 py-2 text-sm font-medium text-center border border-orange-500 shadow rounded-xl">
+      <div className="relative px-4 py-2 text-sm font-medium text-center border border-red-300 shadow bg-red-50 rounded-xl">
         <p>
           Hello, my website is still under development. New changes are uploaded
           every week. Visit the{" "}
@@ -24,6 +24,10 @@ function Index() {
           </a>{" "}
           to learn more.
         </p>
+        <div className="absolute flex -top-0.5 -right-0.5">
+          <span className="absolute w-3 h-3 bg-red-600 border-0 rounded-full shadow-sm animate-ping" />
+          <span className="relative w-3 h-3 bg-red-600 border-0 rounded-full shadow-sm" />
+        </div>
       </div>
       <div className="flex flex-col items-center py-2 gap-y-4 md:justify-between lg:flex-row-reverse">
         <div className="flex items-center justify-center">
@@ -42,24 +46,26 @@ function Index() {
           </div>
         </div>
         <div className="flex flex-col md:max-w-md">
-          <div className="flex animate-gradient-x flex-col gap-y-3 rounded-t-xl border border-b-[0.5px] border-green-600 bg-gradient-to-br from-cyan-100 to-green-200 px-4 py-6 text-center shadow-md shadow-zinc-200 md:text-start">
-            <h1 className="text-2xl font-semibold md:text-3xl">
-              👋Hi, I&apos;m{" "}
-              <span className="text-transparent animate-gradient-x bg-gradient-to-r from-teal-600 to-green-600 bg-clip-text">
-                Chad Fernandez.
-              </span>
-            </h1>
-            <p>
-              I&apos;m an aspiring <b>Python</b> and <b>Web Developer</b>.
-              I&apos;m from the Philippines and a <b>freshman</b> at the
-              University of Antique, <b>studying</b> Bachelor of Science in
-              Computer Science.
-            </p>
+          <div className="rounded-t-xl border border-b-[0.5px] border-green-600 bg-gradient-to-br shadow-md shadow-zinc-200 from-cyan-100 to-green-200 ">
+            <div className="flex flex-col px-4 py-6 text-center border-0 rounded-t-xl animate-gradient-x gap-y-3 md:text-start backdrop-blur-3xl bg-white/0">
+              <h1 className="text-2xl font-semibold md:text-3xl">
+                👋Hi, I&apos;m{" "}
+                <span className="text-transparent animate-gradient-x bg-gradient-to-r from-teal-600 to-green-600 bg-clip-text">
+                  Chad Fernandez.
+                </span>
+              </h1>
+              <p>
+                I&apos;m an aspiring <b>Python</b> and <b>Web Developer</b>.
+                I&apos;m from the Philippines and a <b>freshman</b> at the
+                University of Antique, <b>studying</b> Bachelor of Science in
+                Computer Science.
+              </p>
+            </div>
           </div>
           <div className="flex flex-col w-full md:flex-row">
             {socialLinks.map((social, index) => (
               <a
-                className="px-6 py-2 font-medium bg-white border border-t-0 border-green-600 shadow-md shadow-zinc-200 last:rounded-b-xl hover:bg-sky-100 hover:text-sky-900 md:w-full md:border-0 md:border-r md:border-b md:first:rounded-bl-xl first:md:border-l md:last:rounded-none md:last:rounded-br-xl"
+                className="px-6 py-2 font-medium border border-t-0 border-green-600 shadow-md bg-white/20 backdrop-blur-xl shadow-zinc-200 last:rounded-b-xl hover:bg-sky-100/50 hover:text-blue-600 md:w-full md:border-0 md:border-r md:border-b md:first:rounded-bl-xl first:md:border-l md:last:rounded-none md:last:rounded-br-xl"
                 key={index}
                 href={social.href}
                 target="_blank"
